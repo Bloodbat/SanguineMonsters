67bfe84b117b37419a785479abe973ecd42bbe7f
Bloodbat <bloodbad@prodigy.net.mx>
2024-06-28 08:07:55 -0600

    Fix: C... *rolls eyes*.

--- src/sanguinehelpers.hpp
+++ src/sanguinehelpers.hpp
@@ -1,13 +1,7 @@
 #pragma once
+
 #include "rack.hpp"
 
-math::Vec millimetersToPixelsVec(const float x, const float y) {
-	return mm2px(Vec(x, y));
-}
+rack::math::Vec millimetersToPixelsVec(const float x, const float y);
 
-math::Vec centerWidgetInMillimeters(Widget* theWidget, const float x, const float y) {
-	math::Vec widgetVec;
-	widgetVec = millimetersToPixelsVec(x, y);
-	widgetVec = widgetVec.minus(theWidget->box.size.div(2));
-	return widgetVec;
-}
+rack::math::Vec centerWidgetInMillimeters(rack::widget::Widget* theWidget, const float x, const float y);
