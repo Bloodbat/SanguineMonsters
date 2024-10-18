887198f4998853adcc90d8f75aed42a7d2781877
Bloodbat <bloodbad@prodigy.net.mx>
2024-10-18 12:40:32 -0600

    Refactor: move static constants, enums and structs to header files.

---
+++ src/medusa.hpp
@@ -0,0 +1,9 @@
+#define MEDUSA_MAX_PORTS 32
+
+static const std::vector<RGBLightColor> paletteMedusaLights{
+    { 1.f, 1.f, 1.f },
+    { 1.f, 0.f, 0.f },
+    { 1.f, 1.f, 0.f },
+    { 0.f, 0.f, 1.f },
+    { 0.f, 1.f, 0.f }
+};
