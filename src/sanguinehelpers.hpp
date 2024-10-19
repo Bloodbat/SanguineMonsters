4a906ee771a63fdd4da79c89e2b3942dc70e3e4d
Bloodbat <bloodbad@prodigy.net.mx>
2024-10-19 13:47:40 -0600

    Fix: missing header.

--- src/sanguinehelpers.hpp
+++ src/sanguinehelpers.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "rack.hpp"
-#include <time.h>
+#include <chrono>
 
 rack::math::Vec millimetersToPixelsVec(const float x, const float y);
 
 rack::math::Vec centerWidgetInMillimeters(rack::widget::Widget* theWidget, const float x, const float y);
 
 inline long long getSystemTimeMs() {
-	clock_t now = clock();
-	return (long long)(now * 1000.0 / CLOCKS_PER_SEC);
+	return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
 }
