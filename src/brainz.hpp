5c7a3259b3cf33fdb0f8e4e90252cc947b56ad53
Bloodbat <bloodbad@prodigy.net.mx>
2025-03-16 01:28:18 -0600

    Change: Brainz: better tooltips. Refactor: Brainz: (from TODOs): more constants. Refactor: Brainz: use namespace.

--- src/brainz.hpp
+++ src/brainz.hpp
@@ -1,37 +1,50 @@
 #pragma once
 
-enum ModuleStages {
-    MODULE_STAGE_INIT,
-    MODULE_STAGE_ROUND_1,
-    MODULE_STAGE_ROUND_2,
-    MODULE_STAGE_ONE_SHOT_END,
-    MODULE_STAGE_STATES_COUNT
-};
+namespace brainz {
+    enum ModuleStages {
+        MODULE_STAGE_INIT,
+        MODULE_STAGE_ROUND_1,
+        MODULE_STAGE_ROUND_2,
+        MODULE_STAGE_ONE_SHOT_END,
+        MODULE_STAGE_STATES_COUNT
+    };
 
-enum ModuleStates {
-    MODULE_STATE_READY,
-    MODULE_STATE_ROUND_1_START,
-    MODULE_STATE_ROUND_1_STEP_A,
-    MODULE_STATE_ROUND_1_STEP_B,
-    MODULE_STATE_ROUND_1_STEP_C,
-    MODULE_STATE_ROUND_1_END,
-    MODULE_STATE_ROUND_2_START,
-    MODULE_STATE_ROUND_2_STEP_C,
-    MODULE_STATE_ROUND_2_STEP_B,
-    MODULE_STATE_ROUND_2_STEP_A,
-    MODULE_STATE_ROUND_2_END,
-    MODULE_STATE_WAIT_FOR_RESET
-};
+    enum ModuleStates {
+        MODULE_STATE_READY,
+        MODULE_STATE_ROUND_1_START,
+        MODULE_STATE_ROUND_1_STEP_A,
+        MODULE_STATE_ROUND_1_STEP_B,
+        MODULE_STATE_ROUND_1_STEP_C,
+        MODULE_STATE_ROUND_1_END,
+        MODULE_STATE_ROUND_2_START,
+        MODULE_STATE_ROUND_2_STEP_C,
+        MODULE_STATE_ROUND_2_STEP_B,
+        MODULE_STATE_ROUND_2_STEP_A,
+        MODULE_STATE_ROUND_2_END,
+        MODULE_STATE_WAIT_FOR_RESET
+    };
 
-enum StepStates {
-    STEP_STATE_READY,
-    STEP_STATE_TRIGGER_SENT,
-    STEP_STATE_TRIGGER_DONE
-};
+    enum StepStates {
+        STEP_STATE_READY,
+        STEP_STATE_TRIGGER_SENT,
+        STEP_STATE_TRIGGER_DONE
+    };
 
-enum StepDirections {
-    DIRECTION_BIDIRECTIONAL,
-    DIRECTION_FORWARD,
-    DIRECTION_BACKWARD,
-    DIRECTIONS_COUNT
-};
+    enum StepDirections {
+        DIRECTION_BIDIRECTIONAL,
+        DIRECTION_FORWARD,
+        DIRECTION_BACKWARD,
+        DIRECTIONS_COUNT
+    };
+
+    static const std::vector<std::string> stateToolTips{
+            "Disabled",
+            "Enabled"
+    };
+
+    static const std::vector<std::string> directionToolTips{
+            "Bidirectional",
+            "Forward",
+            "Backward"
+    };
+}
