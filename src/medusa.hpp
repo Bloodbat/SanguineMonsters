18e4d12eb97d3dc12c82adf1e083ccf1af7b4276
Bloodbat <bloodbad@prodigy.net.mx>
2025-03-16 02:12:48 -0600

    Refactor: Medusa: use namespace.

--- src/medusa.hpp
+++ src/medusa.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
-static const int kMedusaMaxPorts = 32;
+namespace medusa {
+    static const int kMedusaMaxPorts = 32;
 
-static const std::vector<RGBLightColor> paletteMedusaLights{
-    { 1.f, 1.f, 1.f },
-    { 1.f, 0.f, 0.f },
-    { 1.f, 1.f, 0.f },
-    { 0.f, 0.f, 1.f },
-    { 0.f, 1.f, 0.f }
-};
+    static const std::vector<RGBLightColor> paletteLights{
+        { 1.f, 1.f, 1.f },
+        { 1.f, 0.f, 0.f },
+        { 1.f, 1.f, 0.f },
+        { 0.f, 0.f, 1.f },
+        { 0.f, 1.f, 0.f }
+    };
+}
