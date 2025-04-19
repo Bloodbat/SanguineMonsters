23038081c71b03890366eac506955bdbea3a0db6
Bloodbat <bloodbad@prodigy.net.mx>
2025-04-19 00:52:13 -0600

    Change: Denki: rework faceplate. Change: Denki: respect color palette.

--- src/denki.cpp
+++ src/denki.cpp
@@ -36,64 +36,64 @@ struct DenkiWidget : SanguineModuleWidget {
 		addChild(createLightCentered<SmallLight<OrangeLight>>(millimetersToPixelsVec(2.769, 5.573), module, Denki::LIGHT_MASTER_MODULE));
 
 		// Channel 1
-		SanguineStaticRGBLight* cvGainLight1 = new SanguineStaticRGBLight(module, "res/light_cv_lit.svg", 6.76, 11.636, true, kSanguineBlueLight);
+		SanguineStaticRGBLight* cvGainLight1 = new SanguineStaticRGBLight(module, "res/light_cv_lit.svg", 6.76, 22.736, true, kSanguineBlueLight);
 		addChild(cvGainLight1);
 
-		SanguineStaticRGBLight* cvOffsetLight1 = new SanguineStaticRGBLight(module, "res/arrow_left_right.svg", 23.72, 11.636, true, kSanguineBlueLight);
+		SanguineStaticRGBLight* cvOffsetLight1 = new SanguineStaticRGBLight(module, "res/arrow_left_right.svg", 23.72, 22.736, true, kSanguineBlueLight);
 		addChild(cvOffsetLight1);
 
-		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(6.76, 20.671), module, Denki::INPUT_GAIN_CV + 0));
+		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(6.76, 29.571), module, Denki::INPUT_GAIN_CV + 0));
 
-		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(23.72, 20.671), module, Denki::INPUT_OFFSET_CV + 0));
+		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(23.72, 29.571), module, Denki::INPUT_OFFSET_CV + 0));
 
-		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(6.76, 30.769), module, Denki::LIGHT_GAIN_CV + 0 * 3));
+		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(6.76, 36.569), module, Denki::LIGHT_GAIN_CV + 0 * 3));
 
-		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(23.72, 30.769), module, Denki::LIGHT_OFFSET_CV + 0 * 3));
+		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(23.72, 36.569), module, Denki::LIGHT_OFFSET_CV + 0 * 3));
 
 		// Channel 2
-		SanguineStaticRGBLight* cvGainLight2 = new SanguineStaticRGBLight(module, "res/light_cv_lit.svg", 6.76, 40.419, true, kSanguineBlueLight);
+		SanguineStaticRGBLight* cvGainLight2 = new SanguineStaticRGBLight(module, "res/light_cv_lit.svg", 6.76, 48.368, true, kSanguineBlueLight);
 		addChild(cvGainLight2);
 
-		SanguineStaticRGBLight* cvOffsetLight2 = new SanguineStaticRGBLight(module, "res/arrow_left_right.svg", 23.72, 40.419, true, kSanguineBlueLight);
+		SanguineStaticRGBLight* cvOffsetLight2 = new SanguineStaticRGBLight(module, "res/arrow_left_right.svg", 23.72, 48.368, true, kSanguineBlueLight);
 		addChild(cvOffsetLight2);
 
-		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(6.76, 49.454), module, Denki::INPUT_GAIN_CV + 1));
+		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(6.76, 55.204), module, Denki::INPUT_GAIN_CV + 1));
 
-		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(23.72, 49.454), module, Denki::INPUT_OFFSET_CV + 1));
+		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(23.72, 55.204), module, Denki::INPUT_OFFSET_CV + 1));
 
-		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(6.76, 59.552), module, Denki::LIGHT_GAIN_CV + 1 * 3));
+		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(6.76, 62.202), module, Denki::LIGHT_GAIN_CV + 1 * 3));
 
-		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(23.72, 59.552), module, Denki::LIGHT_OFFSET_CV + 1 * 3));
+		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(23.72, 62.202), module, Denki::LIGHT_OFFSET_CV + 1 * 3));
 
 		// Channel 3
-		SanguineStaticRGBLight* cvGainLight3 = new SanguineStaticRGBLight(module, "res/light_cv_lit.svg", 6.76, 69.213, true, kSanguineBlueLight);
+		SanguineStaticRGBLight* cvGainLight3 = new SanguineStaticRGBLight(module, "res/light_cv_lit.svg", 6.76, 73.963, true, kSanguineBlueLight);
 		addChild(cvGainLight3);
 
-		SanguineStaticRGBLight* cvOffsetLight3 = new SanguineStaticRGBLight(module, "res/arrow_left_right.svg", 23.72, 69.213, true, kSanguineBlueLight);
+		SanguineStaticRGBLight* cvOffsetLight3 = new SanguineStaticRGBLight(module, "res/arrow_left_right.svg", 23.72, 73.963, true, kSanguineBlueLight);
 		addChild(cvOffsetLight3);
 
-		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(6.76, 78.248), module, Denki::INPUT_GAIN_CV + 2));
+		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(6.76, 80.798), module, Denki::INPUT_GAIN_CV + 2));
 
-		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(23.72, 78.248), module, Denki::INPUT_OFFSET_CV + 2));
+		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(23.72, 80.798), module, Denki::INPUT_OFFSET_CV + 2));
 
-		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(6.76, 88.347), module, Denki::LIGHT_GAIN_CV + 2 * 3));
+		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(6.76, 87.796), module, Denki::LIGHT_GAIN_CV + 2 * 3));
 
-		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(23.72, 88.347), module, Denki::LIGHT_OFFSET_CV + 2 * 3));
+		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(23.72, 87.796), module, Denki::LIGHT_OFFSET_CV + 2 * 3));
 
 		// Channel 4
-		SanguineStaticRGBLight* cvGainLight4 = new SanguineStaticRGBLight(module, "res/light_cv_lit.svg", 6.76, 97.384, true, kSanguineBlueLight);
+		SanguineStaticRGBLight* cvGainLight4 = new SanguineStaticRGBLight(module, "res/light_cv_lit.svg", 6.76, 99.584, true, kSanguineBlueLight);
 		addChild(cvGainLight4);
 
-		SanguineStaticRGBLight* cvOffsetLight4 = new SanguineStaticRGBLight(module, "res/arrow_left_right.svg", 23.72, 97.384, true, kSanguineBlueLight);
+		SanguineStaticRGBLight* cvOffsetLight4 = new SanguineStaticRGBLight(module, "res/arrow_left_right.svg", 23.72, 99.584, true, kSanguineBlueLight);
 		addChild(cvOffsetLight4);
 
 		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(6.76, 106.419), module, Denki::INPUT_GAIN_CV + 3));
 
 		addInput(createInputCentered<BananutPurplePoly>(millimetersToPixelsVec(23.72, 106.419), module, Denki::INPUT_OFFSET_CV + 3));
 
-		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(6.76, 116.517), module, Denki::LIGHT_GAIN_CV + 3 * 3));
+		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(6.76, 113.417), module, Denki::LIGHT_GAIN_CV + 3 * 3));
 
-		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(23.72, 116.517), module, Denki::LIGHT_OFFSET_CV + 3 * 3));
+		addChild(createLightCentered<MediumLight<RedGreenBlueLight>>(millimetersToPixelsVec(23.72, 113.417), module, Denki::LIGHT_OFFSET_CV + 3 * 3));
 	}
 };
 
