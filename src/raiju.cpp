05502c49a37fb4bc922775bb26a3c596451e1133
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2026-05-13 03:56:15 -0600

    Refactor: Raiju: use addOutput instead of addChild when creating outputs.

--- src/raiju.cpp
+++ src/raiju.cpp
@@ -214,18 +214,18 @@ struct RaijuWidget : SanguineModuleWidget {
 		float currentX = 37.073;
 		float xDistance = 12.136;
 		for (int component = 0; component < 4; ++component) {
-			addChild(createOutputCentered<BananutRedPoly>(millimetersToPixelsVec(currentX, 111.758),
+			addOutput(createOutputCentered<BananutRedPoly>(millimetersToPixelsVec(currentX, 111.758),
 				module, Raiju::OUTPUT_VOLTAGE + component));
 			currentX += xDistance;
 		}
 		currentX = 92.018;
 		for (int component = 4; component < 8; ++component) {
-			addChild(createOutputCentered<BananutRedPoly>(millimetersToPixelsVec(currentX, 111.758),
+			addOutput(createOutputCentered<BananutRedPoly>(millimetersToPixelsVec(currentX, 111.758),
 				module, Raiju::OUTPUT_VOLTAGE + component));
 			currentX += xDistance;
 		}
 
-		addChild(createOutputCentered<BananutRedPoly>(millimetersToPixelsVec(82.75, 118.393),
+		addOutput(createOutputCentered<BananutRedPoly>(millimetersToPixelsVec(82.75, 118.393),
 			module, Raiju::OUTPUT_EIGHT_CHANNELS));
 
 		FramebufferWidget* raijuFrameBuffer = new FramebufferWidget();
