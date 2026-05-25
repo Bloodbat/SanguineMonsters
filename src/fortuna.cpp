b2d63df4483c3e14bf3f43c79a238b1f7b184af8
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2026-05-25 12:52:33 -0600

    Refactor: Fortuna: query threshold knobs once per section instead of once per channel per section.

--- src/fortuna.cpp
+++ src/fortuna.cpp
@@ -109,6 +109,8 @@ struct Fortuna : SanguineModule {
 
             float rampDuration = params[PARAM_CROSSFADE_A + section].getValue();
 
+            float knobThreshold = params[PARAM_THRESHOLD_1 + section].getValue();
+
             // Process triggers.
             for (int channel = 0; channel < channelCount; ++channel) {
                 cvVoltages[section][channel] = inputs[INPUT_P_1 + section].getVoltage(channel);
@@ -116,7 +118,7 @@ struct Fortuna : SanguineModule {
                 bool bGatePresent = triggers[section]->getVoltage(channel) >= 1.f;
                 if (btGateTriggers[section][channel].process(bGatePresent)) {
                     // Trigger.
-                    float threshold = clamp(params[PARAM_THRESHOLD_1 + section].getValue() + cvVoltages[section][channel] / 5.f, 0.f, 1.f);
+                    float threshold = clamp(knobThreshold + cvVoltages[section][channel] / 5.f, 0.f, 1.f);
                     rollResults[section][channel] = (random::uniform() >= threshold) ? fortuna::ROLL_HEADS : fortuna::ROLL_TAILS;
                     if (rollModes[section] == fortuna::ROLL_TOGGLE) {
                         rollResults[section][channel] =
